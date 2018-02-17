1d93732972a2c2023271135b36c901c9b71dc9fe
tituspijean <tituspijean@outlook.com>
2018-02-17 19:05:05 +0000

    Implement log out function Logged out user ends up on SSOwat portal though

--- bootstrap.php
+++ bootstrap.php
@@ -1,10 +1,11 @@
 <?php namespace TitusPiJean\Flarum\Auth\SSOwat;
 
-use TitusPiJean\Flarum\Auth\SSOwat\Listener;
+use TitusPiJean\Flarum\Auth\SSOwat\Listeners;
 use Illuminate\Contracts\Events\Dispatcher;
 
 return function (Dispatcher $events) {
     $events->subscribe(Listeners\AddClientAssets::class);
     $events->subscribe(Listeners\AddSSOwatAuthRoute::class);
     $events->subscribe(Listeners\AddApiAttributes::class);
+    $events->subscribe(Listeners\LogOutUser::class);
 };
